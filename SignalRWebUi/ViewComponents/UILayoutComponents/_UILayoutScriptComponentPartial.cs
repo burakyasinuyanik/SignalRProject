716f634db6ae74f600749e9531af92d1260e5a21
Burak Yasin Uyanık <burakyasin3370@gmail.com>
2024-02-10 04:15:56 +0300

    Uı coponent partial eklendi

---
+++ SignalRWebUi/ViewComponents/UILayoutComponents/_UILayoutScriptComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SignalRWebUi.ViewComponents.UILayoutComponents
+{
+    public class _UILayoutScriptComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
