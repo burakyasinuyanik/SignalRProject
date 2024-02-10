7aa79a71263c1ddd868f6aeddd735b9bfedb0bf7
Burak Yasin Uyanık <burakyasin3370@gmail.com>
2024-02-10 19:52:31 +0300

    uı slider ve offer ok

---
+++ SignalRWebUi/ViewComponents/_DefaultComponent/_DefaultSliderComponentPartial.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SignalRWebUi.Dtos.FeatureDtos;
+
+namespace SignalRWebUi.ViewComponents._DefaultComponent
+{
+    public class _DefaultSliderComponentPartial:ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _DefaultSliderComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7294/api/Feature");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultFeatureDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
