058ccf1a09a9ba16c6bc5544a09378951e442df2
Burak Yasin Uyanık <burakyasin3370@gmail.com>
2024-03-10 02:09:41 +0300

    Mesaj Gönderme Ok

--- SignalRWebUi/Controllers/DefaultController.cs
+++ SignalRWebUi/Controllers/DefaultController.cs
@@ -1,12 +1,38 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SignalRWebUi.Dtos.MessageDtos;
+using System.Text;
 
 namespace SignalRWebUi.Controllers
 {
 	public class DefaultController : Controller
 	{
+		private readonly IHttpClientFactory _httpClientFactory;
+
+		public DefaultController(IHttpClientFactory httpClientFactory)
+		{
+			_httpClientFactory = httpClientFactory;
+		}
+
 		public IActionResult Index()
 		{
 			return View();
 		}
+
+		[HttpGet]
+		public IActionResult SendMessage()
+		{
+			return PartialView();
+		}
+		[HttpPost]
+		public async Task< IActionResult> SendMessage(CreateMessageDto createMessageDto)
+		{
+			var client = _httpClientFactory.CreateClient();
+			var jsonData= JsonConvert.SerializeObject(createMessageDto);
+			StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+			await client.PostAsync("https://localhost:7294/api/Message",stringContent);
+			return View();
+		}
+
 	}
 }
