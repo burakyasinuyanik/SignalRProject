d8e4610f3ac7fc2e9cdd3acc12a22ca609ab3316
Burak Yasin Uyanık <burakyasin3370@gmail.com>
2024-03-12 02:08:27 +0300

    mail atma işlemleri tamamlandı

---
+++ SignalRWebUi/Controllers/MailController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+using SignalRWebUi.Dtos.MailDtos;
+using System.Net;
+using System.Net.Mail;
+
+namespace SignalRWebUi.Controllers
+{
+	public class MailController : Controller
+	{
+		[HttpGet]
+		public IActionResult Index()
+		{
+			return View();
+		}
+		[HttpPost]
+		public IActionResult Index(CreateMailDto createMailDto)
+		{
+			MailMessage mailMessage	= new MailMessage("thisrakyazilim@gmail.com", createMailDto.ReceiverEmail,createMailDto.Subject,createMailDto.Body);
+			mailMessage.IsBodyHtml = true;
+
+			SmtpClient smtpClient = new SmtpClient("smtp.gmail.com",587);
+			smtpClient.EnableSsl = true;
+			smtpClient.Credentials = new NetworkCredential("thisrakyazilim@gmail.com", "zbawppdgecuqnwjx");
+			smtpClient.Send(mailMessage);
+
+			return RedirectToAction("Index","Category");
+		}
+	}
+}
---
+++ SignalRWebUi/Dtos/MailDtos/CreateMailDto.cs
@@ -0,0 +1,9 @@
+﻿namespace SignalRWebUi.Dtos.MailDtos
+{
+	public class CreateMailDto
+	{
+		public string ReceiverEmail { get; set; }
+		public string Subject { get; set; }
+		public string Body { get; set; }
+	}
+}
